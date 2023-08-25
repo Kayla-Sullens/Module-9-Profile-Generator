16aaa0e15fa9b8966bece2ae436387e21f95b701
Kayla <sullenspartyof3@gmail.com>
2023-08-25 15:38:51 -0700

    Working prompts and html generation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = () => {
+const generateHTML = ({ name, location, github, linkedin }) => {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -13,12 +13,12 @@ const generateHTML = () => {
     <body>
         <header class="p-5 mb-4 header bg-light">
             <div class="container">
-                <h1 class="display-4">Hi! My name is Kayla.</h1>
-                <p class="lead">I am from Southern California</p>
+                <h1 class="display-4">Hi! My name is ${name}</h1>
+                <p class="lead">I am from ${location}</p>
                 <h3>Contact Me</h3>
                     <ul class="list-group">
-                        <li class="list-group-item">My Github username is: </li>
-                        <li class="list-group-item">LinkedIn: </li>
+                        <li class="list-group-item">My Github username is ${github} </li>
+                        <li class="list-group-item">LinkedIn: ${linkedin} </li>
                     </ul>
             </div>
         </header>
@@ -57,15 +57,13 @@ const init = () => {
         name: 'linkedin',
         message: 'Enter your LinkedIn URL.',
     }]).then((answers) => {
-        console.log(answers);
+        const html = generateHTML(answers);
+
+        const fileName = 'index.html';
+        fs.writeFile(fileName, html, (err) => {
+            err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
+        })
     });
-
-    const html = generateHTML();
-
-    const fileName = 'index.html';
-    fs.writeFile(fileName, html, (err) => {
-        err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
-    })
 }
 
-init();
+    init();
